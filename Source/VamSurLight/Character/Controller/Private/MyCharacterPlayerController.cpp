8785686af2658b46d09bbe37de0ddc84678b3185
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-01-03 18:11:42 +0900

    fix error in controller

--- Source/VamSurLight/Character/Controller/Private/MyCharacterPlayerController.cpp
+++ Source/VamSurLight/Character/Controller/Private/MyCharacterPlayerController.cpp
@@ -1,5 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/Controller/Public/MyCharacterPlayerController.h"
+#include "MyCharacterPlayerController.h"
 
+AMyCharacterPlayerController::AMyCharacterPlayerController() {
+	
+}
+
+void AMyCharacterPlayerController::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void AMyCharacterPlayerController::SetupInputComponent() {
+	Super::SetupInputComponent();
+}
