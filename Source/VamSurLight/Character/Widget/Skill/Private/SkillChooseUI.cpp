9fbb5e87468757da7e209509203478346e475cee
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-01-21 18:25:05 +0900

    feat: limit weapon, status to 3

--- Source/VamSurLight/Character/Widget/Skill/Private/SkillChooseUI.cpp
+++ Source/VamSurLight/Character/Widget/Skill/Private/SkillChooseUI.cpp
@@ -25,38 +25,74 @@ void USkillChooseUI::SetupCards()
 {
 	TArray<FCardOption> AllOptions;
 
-	for (int32 i{static_cast<int32>(EWeaponType::AutoAttack)}; i < static_cast<int32>(EWeaponType::MAX); ++i) {
-		// stop when its 5
-		
-		AActor* FoundActorLevelUpManager = UGameplayStatics::GetActorOfClass(GetWorld(), ALevelUpManager::StaticClass());
-		LevelUpManager = Cast<ALevelUpManager>(FoundActorLevelUpManager);
-		AActor* FoundActor = UGameplayStatics::GetActorOfClass(GetWorld(), ASynergyManager::StaticClass());
-		SynergyManager = Cast<ASynergyManager>(FoundActor);
-		
-		if (LevelUpManager->GetWeaponLevel(static_cast<EWeaponType>(i)) < 4) {
-			FCardOption Option;
-			Option.WeaponType = static_cast<EWeaponType>(i);
-			Option.bIsWeapon = true;
-			AllOptions.Add(Option);
+	AActor* FoundActorLevelUpManager = UGameplayStatics::GetActorOfClass(GetWorld(), ALevelUpManager::StaticClass());
+	LevelUpManager = Cast<ALevelUpManager>(FoundActorLevelUpManager);
+	AActor* FoundActor = UGameplayStatics::GetActorOfClass(GetWorld(), ASynergyManager::StaticClass());
+	SynergyManager = Cast<ASynergyManager>(FoundActor);
+
+	LogUtils::Log("111111111111", SynergyManager->WeaponBitmask);
+	if (3 == SynergyManager->GetWeaponCount()) {
+		TArray<int32> Weapons{SynergyManager->FindSetWeapons()};
+		for (int32 Index : Weapons) {
+			if (LevelUpManager->GetWeaponLevel(static_cast<EWeaponType>(Index)) < 4) {
+				FCardOption Option;
+				Option.WeaponType = static_cast<EWeaponType>(Index);
+				Option.bIsWeapon = true;
+				AllOptions.Add(Option);
+			}
+			if (LevelUpManager->GetWeaponLevel(static_cast<EWeaponType>(Index)) == 4 && SynergyManager->CheckSynergy(
+				static_cast<EWeaponType>(Index))) {
+				FCardOption Option;
+				Option.WeaponType = static_cast<EWeaponType>(Index);
+				Option.bIsWeapon = true;
+				AllOptions.Add(Option);
+			}
 		}
-		// check synergy if its 4
-		if (LevelUpManager->GetWeaponLevel(static_cast<EWeaponType>(i)) == 4 && SynergyManager->CheckSynergy(static_cast<EWeaponType>(i))) {
-			FCardOption Option;
-			Option.WeaponType = static_cast<EWeaponType>(i);
-			Option.bIsWeapon = true;
-			AllOptions.Add(Option);
+	}
+	else {
+		for (int32 i{static_cast<int32>(EWeaponType::AutoAttack)}; i < static_cast<int32>(EWeaponType::MAX); ++i) {
+			// stop when its 5
+
+			if (LevelUpManager->GetWeaponLevel(static_cast<EWeaponType>(i)) < 4) {
+				FCardOption Option;
+				Option.WeaponType = static_cast<EWeaponType>(i);
+				Option.bIsWeapon = true;
+				AllOptions.Add(Option);
+			}
+			// check synergy if its 4
+			if (LevelUpManager->GetWeaponLevel(static_cast<EWeaponType>(i)) == 4 && SynergyManager->CheckSynergy(
+				static_cast<EWeaponType>(i))) {
+				FCardOption Option;
+				Option.WeaponType = static_cast<EWeaponType>(i);
+				Option.bIsWeapon = true;
+				AllOptions.Add(Option);
+			}
 		}
 	}
 
-	for (int32 i{static_cast<int32>(EStatusType::CoolTimeUpdate)}; i < static_cast<int32>(EStatusType::MAX); ++i) {
-		// stop when its 5
-		if (LevelUpManager->GetStatusLevel(static_cast<EStatusType>(i)) == 5) {
-			continue;
+	if (3 == SynergyManager->GetStatusCount()) {
+		TArray<int32> Status{SynergyManager->FindSetStatus()};
+		for (int32 Index : Status) {
+			if (LevelUpManager->GetStatusLevel(static_cast<EStatusType>(Index)) == 5) {
+				continue;
+			}
+			FCardOption Option;
+			Option.StatusType = static_cast<EStatusType>(Index);
+			Option.bIsWeapon = false;
+			AllOptions.Add(Option);
 		}
-		FCardOption Option;
-		Option.StatusType = static_cast<EStatusType>(i);
-		Option.bIsWeapon = false;
-		AllOptions.Add(Option);
+	}
+	else {
+		for (int32 i{static_cast<int32>(EStatusType::CoolTimeUpdate)}; i < static_cast<int32>(EStatusType::MAX); ++i) {
+			// stop when its 5
+			if (LevelUpManager->GetStatusLevel(static_cast<EStatusType>(i)) == 5) {
+				continue;
+			}
+			FCardOption Option;
+			Option.StatusType = static_cast<EStatusType>(i);
+			Option.bIsWeapon = false;
+			AllOptions.Add(Option);
+		}
 	}
 
 	CardOptions.Empty();
